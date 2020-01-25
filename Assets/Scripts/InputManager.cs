33a2d0d42140084209743d0f5b02eab617a538cb
JSymons7 <jsym922@aucklanduni.ac.nz>
2020-01-26 12:13:34 +1300

    Initial game stage
    Not adding metadata at this stage because it's quite large. Don't think Temp is needed, so I'll remove that as well. Will add them in down the line if necessary. May also end up removing more stuff that isn't necessary.

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public float panSpeed;
+    public float rotateSpeed;
+    public float rotateAmount;
+
+    private Quaternion rotation;
+
+    private float panDetect = 15f;
+    private float minHeight = 10f;
+    private float maxHeight = 100f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotation = Camera.main.transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    void MoveCamera()
+    {
+        float moveX = Camera.main.transform.position.x;
+        float moveZ = Camera.main.transform.position.z;
+
+        float xPos = Input.mousePosition.x;
+        float yPos = Input.mousePosition.y;
+
+        if (Input.GetKey(KeyCode.A) || xPos > 0 && xPos < panDetect)
+        {
+            moveX -= panSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D) || xPos < Screen.width && xPos > Screen.width - panDetect)
+        {
+            moveX += panSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.W) || yPos < Screen.height && yPos > Screen.height - panDetect)
+        {
+            moveZ += panSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S) || yPos > 0 && yPos < panDetect)
+        {
+            moveZ -= panSpeed;
+        }
+
+        Vector3 newPos = new Vector3(moveX, 0, moveZ);
+
+        Camera.main.transform.position = newPos;
+
+    }
+}
